9cdffae839d2d4354034d659eae05ff4a7fd1584
Pavel Svitek <pavel.svitek@kistler.com>
2016-12-01 17:37:06 +0100

    Added babel.rc, webpack.config.js, src/, modified dist/index.html

---
+++ src/components/app.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Initial Chat-App wrapper.
+      </div>
+    )
+  }
+}
+
+export default App;
