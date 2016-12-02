23d399bb49b87c6b358a4ad1578f3d51c61e4ea8
Pavel Svitek <pavel.svitek@kistler.com>
2016-12-02 10:00:45 +0100

    Added pkg horizon/client, refactor code to use database, added basic event handlers

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,18 +1,54 @@
 import React, { Component } from 'react';
+import Horizon from '@horizon/client';
 import Messages from './messages';
 
+const horizon = Horizon({ secure: false });
+const chat = horizon('messages');
+
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      author: '',
+      text: ''
+    }
+  }
+
+  handleChangeField(key, event) {
+    this.setState({
+      [key]: event.target.value
+    });
+  }
+
+  sendMessage(event) {
+    event.preventDefault();
+    if (!this.state.author || !this.state.text){
+      alert('Author and message must be specified!');
+      return;
+    }
+
+    const message = {
+      author: this.state.author,
+      text: this.state.text,
+    };
+
+    // Store the message and reset the text field
+    chat.store(message);
+    this.setState({ text: '' });
+  }
+
   render() {
     return (
       <div>
         <form>
           <div className="center">
-            <button>Send Message</button>
-            <input placeholder="Author"></input>
-            <input placeholder="Hey there.."></input>
+            <input onChange={this.handleChangeField.bind(this, 'author')} value={this.state.author} placeholder="Author"></input>
+            <input onChange={this.handleChangeField.bind(this, 'text')} value={this.state.text} placeholder="Hey there.."></input>
+            <button onClick={this.sendMessage.bind(this)}>Send Message</button>
           </div>
         </form>
-        <Messages />
+        <Messages chat={chat}/>
       </div>
     )
   }
--- src/components/messages.jsx
+++ src/components/messages.jsx
@@ -5,12 +5,23 @@ import Message from './message';
 class Messages extends Component {
   constructor(props) {
     super(props);
+    this.chat = props.chat;
     this.state = {
-      conversation: [{text: 'Hi John!', author: '@svitekpavel'},
-              {text: 'Hi there!', author: '@johndoe'},
-              {text: 'Whats up with you?', author: '@svitekpavel'},
-              {text: "I'm good .. don't worry!", author: '@johndoe'}]
+      conversation: []
      };
+   }
+
+   componentDidMount() {
+     this.chat.watch().subscribe(
+       (messages) => {
+         const conversation = messages.map((message) => {
+           return message;
+         });
+         this.setState({
+           conversation: messages
+         });
+       }
+     );
    }
 
    render() {
