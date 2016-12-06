c8b320a103ef71e92cd5551d258c510051872fbe
Pavel Svitek <svitek.pavel@gmail.com>
2016-12-06 21:26:30 +0100

    Removed constructor from Message component

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 
 export default class Message extends Component {
-  constructor(props) {
-    super(props);
-    this.props = props;
-  }
   render() {
     const message = this.props.message;
     return (
