73ce76e1b264cb0c91e74b95404fd44a7eb01f84
Pavel Svitek <svitek.pavel@gmail.com>
2016-12-06 21:26:51 +0100

    Refactored MessageForm component

--- src/components/message-form.jsx
+++ src/components/message-form.jsx
@@ -12,9 +12,21 @@ export default class MessageForm extends Component {
     return (
       <form ref="form">
         <div className="container">
-          <TextField onChange={this.props.onAuthorChange} value={this.props.author} floatingLabelText="Author" hintText="Your name"/>
-          <TextField onChange={this.props.onTextChange} value={this.props.text} floatingLabelText="Message" hintText="Hey there.."/>
-          <RaisedButton secondary={true} onTouchTap={this.props.onSendMessage} style={styleButton}>Send Message</RaisedButton>
+          <TextField
+            onChange={this.props.onAuthorChange}
+            value={this.props.author}
+            floatingLabelText="Author"
+            hintText="Your name"/>
+          <TextField
+            onChange={this.props.onTextChange}
+            value={this.props.text}
+            floatingLabelText="Message"
+            hintText="Hey there.."/>
+          <RaisedButton
+            primary={true}
+            onTouchTap={this.props.onSendMessage}
+            style={styleButton}
+            >Send Message</RaisedButton>
         </div>
       </form>
     )
