bd5dcca3b9fb08b1c5fdae0f9264ec1bacf864a7
Pavel Svitek <svitek.pavel@gmail.com>
2016-12-22 10:29:13 +0100

    added action `storeFormData`

--- src/actions.js
+++ src/actions.js
@@ -1,4 +1,5 @@
 export const ADD_MESSAGE = 'ADD_MESSAGE'
+export const STORE_FORM_DATA = 'STORE_FORM_DATA'
 
 export const createAction = (actionType, payload = {}) => {
   return {
@@ -8,3 +9,4 @@ export const createAction = (actionType, payload = {}) => {
 }
 
 export const addMessage = (payload) => createAction(ADD_MESSAGE, payload);
+export const storeFormData = (payload) => createAction(STORE_FORM_DATA, payload);
